463b223cb397f76e6a5ce703d7eab4f8afef1403
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-08-08 22:20:58 +0200

    Implement build command

--- build.go
+++ build.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/Bananenpro/cli"
 	"github.com/code-game-project/go-utils/cgfile"
+	"github.com/code-game-project/go-utils/exec"
 	"github.com/code-game-project/go-utils/modules"
+	cp "github.com/otiai10/copy"
 )
 
 func Build() error {
-	panic("not implemented")
 	config, err := cgfile.LoadCodeGameFile("")
 	if err != nil {
 		return err
@@ -18,21 +23,115 @@ func Build() error {
 	if err != nil {
 		return err
 	}
+	if data.Output == "" {
+		data.Output = "build"
+	}
+
+	typescript := data.Lang == "ts"
+
+	runtime, _ := config.LangConfig["runtime"].(string)
+	if runtime != "node" && runtime != "bundler" && (typescript || runtime != "browser") {
+		return fmt.Errorf("Invalid runtime: '%s'", runtime)
+	}
 
 	switch config.Type {
 	case "client":
-		return buildClient(config.Game, data.Output, config.URL)
+		return buildClient(config.Game, data.Output, config.URL, typescript, runtime)
 	case "server":
-		return buildServer(data.Output)
+		return buildServer()
 	default:
 		return fmt.Errorf("Unknown project type: %s", config.Type)
 	}
 }
 
-func buildClient(gameName, output, url string) error {
+func buildClient(gameName, output, url string, typescript bool, runtime string) error {
+	yes, err := cli.YesNo(fmt.Sprintf("The '%s' directory will be completely overridden. Continue?", output), false)
+	if err != nil || !yes {
+		return cli.ErrCanceled
+	}
+	os.RemoveAll(output)
+	err = os.MkdirAll(output, 0o755)
+	if err != nil {
+		return fmt.Errorf("Failed to create output directory: %w", err)
+	}
+
+	cli.BeginLoading("Building...")
+
+	if runtime == "node" {
+		if typescript {
+			_, err = exec.Execute(true, "npx", "tsc", "--outDir", output)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = cp.Copy("src", output, cp.Options{
+				OnSymlink: func(src string) cp.SymlinkAction {
+					return cp.Deep
+				},
+			})
+			if err != nil {
+				return fmt.Errorf("Failed to copy source files to output directory: %s", err)
+			}
+		}
+	} else if runtime == "bundler" {
+		gameJSPath := filepath.Join("src", gameName, "game.js")
+		if typescript {
+			gameJSPath = filepath.Join("src", gameName, "game.ts")
+		}
+		err = replaceInFile(gameJSPath, "throw 'Query parameter \"game_url\" must be set.'", fmt.Sprintf("return '%s'", url))
+		if err != nil {
+			return err
+		}
+
+		_, err = exec.Execute(true, "npx", "parcel", "build", "--dist-dir", output, "src/index.html")
+		if err != nil {
+			return err
+		}
+
+		err = replaceInFile(gameJSPath, fmt.Sprintf("return '%s'", url), "throw 'Query parameter \"game_url\" must be set.'")
+		if err != nil {
+			return err
+		}
+	} else if runtime == "browser" {
+		err = cp.Copy(".", output, cp.Options{
+			Skip: func(src string) (bool, error) {
+				return src == "build" || (src != "node_modules" && strings.HasPrefix(src, "node_modules") && !strings.Contains(src, "@code-game-project")) || src == ".codegame.json" || src == "package.json" || src == "package-lock.json", nil
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("Failed to copy source files to output directory: %s", err)
+		}
+	}
+
+	if runtime != "bundler" {
+		gameJSPath := filepath.Join(output, gameName, "game.js")
+		if runtime == "node" {
+			err = replaceInFile(gameJSPath, "throw 'Environment variable `CG_GAME_URL` must be set.'", fmt.Sprintf("return '%s'", url))
+		} else {
+			err = replaceInFile(gameJSPath, "throw 'Query parameter \"game_url\" must be set.'", fmt.Sprintf("return '%s'", url))
+		}
+	}
+	if err != nil {
+		return err
+	}
+	cli.FinishLoading()
 	return nil
 }
 
-func buildServer(output string) error {
+func replaceInFile(filename, old, new string) error {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("Failed to replace '%s' with '%s' in '%s': %s", old, new, filename, err)
+	}
+	content = []byte(strings.ReplaceAll(string(content), old, new))
+	err = os.WriteFile(filename, content, 0o644)
+	if err != nil {
+		return fmt.Errorf("Failed to replace '%s' with '%s' in '%s': %s", old, new, filename, err)
+	}
+	return nil
+}
+
+func buildServer() error {
+	panic("not implemented")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/otiai10/copy v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 	golang.org/x/text v0.3.7 // indirect
