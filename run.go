bca12c7cd8eead70f5549489412c0c775724a3c3
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-08-08 16:39:44 +0200

    Wait for dev server to come online

--- run.go
+++ run.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"strings"
@@ -78,86 +79,96 @@ func runClient(url string, args []string, runtime string, typescript bool) error
 			return fmt.Errorf("Failed to run 'CG_GAME_URL=%s node %s'.", url, strings.Join(cmdArgs, " "))
 		}
 	} else {
-		if _, err := exec.LookPath("npx"); err != nil {
-			return fmt.Errorf("'npx' ist not installed!")
-		}
 		var cmdArgs []string
 		if runtime == "bundler" {
 			cmdArgs = []string{"parcel", "--watch-for-stdin", "-p", "5000", "src/index.html"}
 		} else {
 			cmdArgs = []string{"serve", "-n", "--no-port-switching", "-p", "5000", "."}
 		}
-		cmd := exec.Command("npx", cmdArgs...)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
 
-		err := cmd.Start()
-		if err != nil {
-			return fmt.Errorf("Failed to start '%s': %s", strings.Join(cmdArgs, " "), err)
-		}
-
-		pflag.Usage = func() {
-			fmt.Fprintf(os.Stdout, "Usage: %s [options] [command]\n\n", os.Args[0])
-			fmt.Fprintf(os.Stdout, "Options:\n")
-			pflag.PrintDefaults()
-			fmt.Fprintf(os.Stdout, "\nCommands:\n")
-			fmt.Fprintf(os.Stdout, "  create [options] <username>                        Create and join a new game.\n")
-			fmt.Fprintf(os.Stdout, "  join [options] <game_id> <username> [join_secret]  Join an existing game.\n")
-			fmt.Fprintf(os.Stdout, "  reconnect [options] <username>                     Reconnect to a game using a saved session.\n")
-			fmt.Fprintf(os.Stdout, "  spectate [options] [game_id]                       Spectate a new or existing game.\n")
-		}
-
-		var queryParams string
-		var public bool
-		pflag.BoolVar(&public, "public", false, "Make the created game protected.")
-		var protected bool
-		pflag.BoolVar(&public, "protected", false, "Make the created game protected.")
-		pflag.CommandLine.Parse(args)
-
-		var op string
-		if pflag.NArg() > 0 {
-			op = pflag.Arg(0)
-		}
-
-		switch op {
-		case "create", "reconnect":
-			if pflag.NArg() > 1 {
-				queryParams += "&username=" + pflag.Arg(1)
+		done := make(chan struct{})
+		runWhenUP("localhost:5000", func() {
+			pflag.Usage = func() {
+				fmt.Fprintf(os.Stdout, "Usage: %s [options] [command]\n\n", os.Args[0])
+				fmt.Fprintf(os.Stdout, "Options:\n")
+				pflag.PrintDefaults()
+				fmt.Fprintf(os.Stdout, "\nCommands:\n")
+				fmt.Fprintf(os.Stdout, "  create [options] <username>                        Create and join a new game.\n")
+				fmt.Fprintf(os.Stdout, "  join [options] <game_id> <username> [join_secret]  Join an existing game.\n")
+				fmt.Fprintf(os.Stdout, "  reconnect [options] <username>                     Reconnect to a game using a saved session.\n")
+				fmt.Fprintf(os.Stdout, "  spectate [options] [game_id]                       Spectate a new or existing game.\n")
 			}
-		case "join":
-			if pflag.NArg() > 1 {
-				queryParams += "&game_id=" + pflag.Arg(1)
-			}
-			if pflag.NArg() > 2 {
-				queryParams += "&username=" + pflag.Arg(2)
-			}
-			if pflag.NArg() > 3 {
-				queryParams += "&join_secret=" + pflag.Arg(3)
-			}
-		case "spectate":
-			if pflag.NArg() > 1 {
-				queryParams += "&game_id=" + pflag.Arg(1)
-			}
-		}
 
-		if public {
-			queryParams += "&public=true"
-		}
-		if protected {
-			queryParams += "&protected=true"
-		}
+			var queryParams string
+			var public bool
+			pflag.BoolVar(&public, "public", false, "Make the created game protected.")
+			var protected bool
+			pflag.BoolVar(&public, "protected", false, "Make the created game protected.")
+			pflag.CommandLine.Parse(args)
 
-		time.Sleep(2 * time.Second)
+			var op string
+			if pflag.NArg() > 0 {
+				op = pflag.Arg(0)
+			}
 
-		cgExec.OpenBrowser(fmt.Sprintf("http://localhost:5000?game_url=%s&op=%s%s", url, op, queryParams))
-		err = cmd.Wait()
+			switch op {
+			case "create", "reconnect":
+				if pflag.NArg() > 1 {
+					queryParams += "&username=" + pflag.Arg(1)
+				}
+			case "join":
+				if pflag.NArg() > 1 {
+					queryParams += "&game_id=" + pflag.Arg(1)
+				}
+				if pflag.NArg() > 2 {
+					queryParams += "&username=" + pflag.Arg(2)
+				}
+				if pflag.NArg() > 3 {
+					queryParams += "&join_secret=" + pflag.Arg(3)
+				}
+			case "spectate":
+				if pflag.NArg() > 1 {
+					queryParams += "&game_id=" + pflag.Arg(1)
+				}
+			}
+
+			if public {
+				queryParams += "&public=true"
+			}
+			if protected {
+				queryParams += "&protected=true"
+			}
+
+			cgExec.OpenBrowser(fmt.Sprintf("http://localhost:5000?game_url=%s&op=%s%s", url, op, queryParams))
+		}, done)
+
+		_, err := cgExec.Execute(false, "npx", cmdArgs...)
+		close(done)
 		if err != nil {
 			return fmt.Errorf("Failed to run '%s': %s", strings.Join(cmdArgs, " "), err)
 		}
 	}
 
 	return nil
+}
+
+func runWhenUP(address string, fn func(), done <-chan struct{}) {
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			con, err := net.Dial("tcp", address)
+			if err == nil {
+				con.Close()
+				break
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+		fn()
+	}()
 }
 
 func runServer(args []string, typescript bool) error {
